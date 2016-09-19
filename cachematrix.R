e12e4df2b004a086d7a31c7f894611dffb421624
R. Chikanya <R. Chikanya>
2016-09-19 23:15:22 +0200

    updated cachematrix.R and README.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,37 @@
-## The programming assignment is to cache the invertible matrix for later use without computed repeatedly (if not necessary). 
+## The following functions work together to create a square invertible matrix
+## and make the inverse of the matrix available in the cache environment. 
 ## This feature is inherited from the 'Scoping rule' of R.
-## The first function is to create a 
 
 makeCacheMatrix <- function(x = matrix()) {
   ## This function creates a special "matrix" object that can cache its inverse.
   inv <- NULL
-  set <- function(y){
+  set <- function(y) {
     x <<- y
     inv <<- NULL
   }
   get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  setInverse <- function(inverse) inv <<- inverse
   getInverse <- function() inv
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-##the cacheSove will retrieve the inverted matrix in the cache and skips
-## computation. Otherwise, it solve the inverted matrix of the data and 
-## set to the inv in the cache via the setinv function
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
-  ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-  inv <- x$getInverse()
   ## Return a matrix that is the inverse of 'x'
-  if(!is.null(inv)){
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
-  
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
